4ee2892515c9e14bf01aeb10e6749eca37a9da29
brminnick <brandon.r.minnick@gmail.com>
2017-07-25 14:22:44 -0700

    First Commit

---
+++ XamList/App.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamList
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage();
+        }
+    }
+}
