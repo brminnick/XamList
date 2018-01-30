58e0dcf451c8fa949b3453740bce51f3871a5f71
brminnick <brandon.r.minnick@gmail.com>
2018-01-30 14:28:02 -0800

    Added Lazy Initialization

--- XamList/Data/Base/BaseDatabase.cs
+++ XamList/Data/Base/BaseDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 using SQLite;
 
@@ -11,28 +12,31 @@ namespace XamList
     public abstract class BaseDatabase
     {
         #region Constant Fields
-        static readonly SQLiteAsyncConnection _databaseConnection = DependencyService.Get<ISQLite>().GetConnection();
+        static readonly Lazy<SQLiteAsyncConnection> _databaseConnectionHolder = new Lazy<SQLiteAsyncConnection>(DependencyService.Get<ISQLite>().GetConnection);
         #endregion
 
         #region Fields
         static bool _isInitialized;
         #endregion
 
+        #region Properties
+        static SQLiteAsyncConnection DatabaseConnection => _databaseConnectionHolder.Value;
+        #endregion
+
         #region Methods
         protected static async ValueTask<SQLiteAsyncConnection> GetDatabaseConnectionAsync()
         {
             if (!_isInitialized)
-				await Initialize();
-                
-            return _databaseConnection;
+                await Initialize();
+
+            return DatabaseConnection;
         }
 
         static async Task Initialize()
         {
-            await _databaseConnection.CreateTableAsync<ContactModel>();
+            await DatabaseConnection.CreateTableAsync<ContactModel>();
             _isInitialized = true;
         }
         #endregion
-
     }
 }
