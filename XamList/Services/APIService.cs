a292d4de83078cb53d8d0eb2cc49c9916bd24498
Brandon Minnick <brminnick@users.noreply.github.com>
2018-04-23 17:29:24 -0700

    Updated DeleteContactModel API

--- XamList/Services/APIService.cs
+++ XamList/Services/APIService.cs
@@ -6,26 +6,26 @@ using XamList.Shared;
 
 namespace XamList
 {
-    abstract class APIService : BaseHttpClientService
-    {
-        #region Methods
-        public static Task<List<ContactModel>> GetAllContactModels() =>
-            GetObjectFromAPI<List<ContactModel>>($"{BackendConstants.AzureAPIUrl}GetAllContacts");
+	abstract class APIService : BaseHttpClientService
+	{
+		#region Methods
+		public static Task<List<ContactModel>> GetAllContactModels() =>
+			GetObjectFromAPI<List<ContactModel>>($"{BackendConstants.AzureAPIUrl}GetAllContacts");
 
-        public static Task<ContactModel> GetContactModel(string id) =>
-		    GetObjectFromAPI<ContactModel>($"{BackendConstants.AzureAPIUrl}GetContact/{id}");
+		public static Task<ContactModel> GetContactModel(string id) =>
+			GetObjectFromAPI<ContactModel>($"{BackendConstants.AzureAPIUrl}GetContact/{id}");
 
-        public static Task<HttpResponseMessage> PostContactModel(ContactModel contact) =>
-            PostObjectToAPI($"{BackendConstants.AzureAPIUrl}PostContact", contact);
+		public static Task<HttpResponseMessage> PostContactModel(ContactModel contact) =>
+			PostObjectToAPI($"{BackendConstants.AzureAPIUrl}PostContact", contact);
 
-        public static Task<HttpResponseMessage> PatchContactModel(ContactModel contact) =>
-            PatchObjectToAPI($"{BackendConstants.AzureAPIUrl}PatchContact/{contact.Id}", contact);
+		public static Task<HttpResponseMessage> PatchContactModel(ContactModel contact) =>
+			PatchObjectToAPI($"{BackendConstants.AzureAPIUrl}PatchContact/{contact.Id}", contact);
 
-        public static Task<HttpResponseMessage> DeleteContactModel(ContactModel contact) =>
-            DeleteObjectFromAPI($"{BackendConstants.AzureAPIUrl}DeleteContact/{contact.Id}");
+		public static Task<HttpResponseMessage> DeleteContactModel(string id) =>
+			DeleteObjectFromAPI($"{BackendConstants.AzureAPIUrl}DeleteContact/{id}");
 
-        public static Task<HttpResponseMessage> RestoreDeletedContacts() =>
-            PostObjectToAPI($"{BackendConstants.AzureFunctionUrl}RestoreDeletedContacts/?code={BackendConstants.AzureFunctionKey_RestoreDeletedContacts}", new object());
-        #endregion
-    }
+		public static Task<HttpResponseMessage> RestoreDeletedContacts() =>
+			PostObjectToAPI($"{BackendConstants.AzureFunctionUrl}RestoreDeletedContacts/?code={BackendConstants.AzureFunctionKey_RestoreDeletedContacts}", new object());
+		#endregion
+	}
 }
--- XamList/Views/ContactsListTextCell.cs
+++ XamList/Views/ContactsListTextCell.cs
@@ -8,75 +8,73 @@ using XamList.Shared;
 
 namespace XamList
 {
-    public class ContactsListTextCell : TextCell
-    {
-        #region Constant Fields
-        readonly MenuItem _deleteAction;
-        #endregion
+	public class ContactsListTextCell : TextCell
+	{
+		#region Constant Fields
+		readonly MenuItem _deleteAction;
+		#endregion
 
-        #region Constructors
-        public ContactsListTextCell()
-        {
-            TextColor = ColorConstants.TextColor;
-            DetailColor = ColorConstants.DetailColor;
+		#region Constructors
+		public ContactsListTextCell()
+		{
+			TextColor = ColorConstants.TextColor;
+			DetailColor = ColorConstants.DetailColor;
 
-            _deleteAction = new MenuItem
-            {
-                Text = "Delete",
-                IsDestructive = true
-            };
-            _deleteAction.Clicked += HandleDeleteClicked;
+			_deleteAction = new MenuItem
+			{
+				Text = "Delete",
+				IsDestructive = true
+			};
+			_deleteAction.Clicked += HandleDeleteClicked;
 
-            ContextActions.Add(_deleteAction);
-        }
-        #endregion
+			ContextActions.Add(_deleteAction);
+		}
+		#endregion
 
-        #region Finalizers
-        ~ContactsListTextCell()
-        {
-            ContextActions.Remove(_deleteAction);
-            _deleteAction.Clicked -= HandleDeleteClicked;
-        }
-        #endregion
+		#region Finalizers
+		~ContactsListTextCell()
+		{
+			ContextActions.Remove(_deleteAction);
+			_deleteAction.Clicked -= HandleDeleteClicked;
+		}
+		#endregion
 
-        #region Properties
-        ContactsListPage ContactsListPage
-        {
-            get
-            {
-                var navigationPage = Application.Current.MainPage as NavigationPage;
-                return navigationPage.Navigation.NavigationStack.FirstOrDefault() as ContactsListPage;
-            }
-        }
+		#region Properties
+		ContactsListViewModel ContactsListViewModel => ContactsListPage.BindingContext as ContactsListViewModel;
 
-        ContactsListViewModel ContactsListViewModel => 
-            ContactsListPage.BindingContext as ContactsListViewModel;
+		ContactsListPage ContactsListPage
+		{
+			get
+			{
+				var navigationPage = Application.Current.MainPage as NavigationPage;
+				return navigationPage.Navigation.NavigationStack.FirstOrDefault() as ContactsListPage;
+			}
+		}
+		#endregion
 
-        #endregion
+		#region Methods
+		protected override void OnBindingContextChanged()
+		{
+			base.OnBindingContextChanged();
 
-        #region Methods
-        protected override void OnBindingContextChanged()
-        {
-            base.OnBindingContextChanged();
+			Text = string.Empty;
+			Detail = string.Empty;
 
-            Text = string.Empty;
-            Detail = string.Empty;
+			var item = BindingContext as ContactModel;
 
-            var item = BindingContext as ContactModel;
+			Text = item.FullName;
+			Detail = item.PhoneNumber;
+		}
 
-            Text = item.FullName;
-            Detail = item.PhoneNumber;
-        }
+		async void HandleDeleteClicked(object sender, EventArgs e)
+		{
+			var contactSelected = BindingContext as ContactModel;
 
-        async void HandleDeleteClicked(object sender, EventArgs e)
-        {
-            var contactSelected = BindingContext as ContactModel;
+			await Task.WhenAll(APIService.DeleteContactModel(contactSelected?.Id),
+							   ContactDatabase.DeleteContact(contactSelected)).ConfigureAwait(false);
 
-            await Task.WhenAll(APIService.DeleteContactModel(contactSelected), 
-                               ContactDatabase.DeleteContact(contactSelected));
-
-            ContactsListViewModel.RefreshCommand?.Execute(null);
-        }
-        #endregion
-    }
+			ContactsListViewModel.RefreshCommand?.Execute(null);
+		}
+		#endregion
+	}
 }
