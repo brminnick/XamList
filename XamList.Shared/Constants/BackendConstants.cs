6e9660c0945aca8e6cdce3b29eb9673e44b4fc3b
Brandon Minnick <brminnick@users.noreply.github.com>
2018-10-08 11:28:24 -0700

    Updated URLs for .NET Core Backend

--- XamList.Shared/Constants/BackendConstants.cs
+++ XamList.Shared/Constants/BackendConstants.cs
@@ -3,9 +3,9 @@
 #warning: To utilize your own Azure API and Azure Function, replace the values of these strings
     public static class BackendConstants
     {
-        public const string AzureAPIUrl = "https://xamlistapicore.azurewebsites.net/api/";
+        public const string AzureAPIUrl = "https://xamlistapiapp.azurewebsites.net/api/";
         public const string AzureFunctionUrl = "https://xamlistfunctions2.azurewebsites.net/";
-        public const string AzureFunctionKey_RemoveItemFromDatabase = "12rEQESsyX2qOw0aKgK2WQ8mgjDbdp6XKiwPNCM04WyXCY9Ep9Wr5A==";
-        public const string AzureFunctionKey_RestoreDeletedContacts = "O/6QQypqCzsJgdh5s6KxNkE393tksrZLePK/eszd4JwHPFkHmuI7nA==";
+        public const string AzureFunctionKey_RemoveItemFromDatabase = @"12rEQESsyX2qOw0aKgK2WQ8mgjDbdp6XKiwPNCM04WyXCY9Ep9Wr5A==";
+        public const string AzureFunctionKey_RestoreDeletedContacts = @"O/6QQypqCzsJgdh5s6KxNkE393tksrZLePK/eszd4JwHPFkHmuI7nA==";
     }
 }
