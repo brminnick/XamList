95cb6116a0672da9034e7feff66284a16087f0e4
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-01-02 17:27:05 -0800

    Update NuGet Packages

--- XamList.Backend.Shared/Database/XamListDatabase.cs
+++ XamList.Backend.Shared/Database/XamListDatabase.cs
@@ -118,6 +118,8 @@ namespace XamList.Backend.Shared
 
         class XamListDatabaseContext : DbContext
         {
+            public XamListDatabaseContext() => Database.EnsureCreated();
+
             public DbSet<ContactModel>? Contacts { get; set; }
 
             protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(_connectionString);
